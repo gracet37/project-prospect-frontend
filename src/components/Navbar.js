0e1d91fa2199af5e8a9ff9655230afd8e695569e
github username <github email address>
2019-09-20 12:07:30 -0500

    added navbar and links

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+const styleBar = {
+  overflow: 'hidden',
+  backgroundColor: '#7E39FB'
+  // position: 'fixed',
+  // top: '0',
+  // width: '100%'
+
+}
+
+const styleLink = {
+  float: 'right',
+  display: 'block',
+  color: '#f2f2f2',
+  textAlign: 'center',
+  padding: '14px 16px',
+  fontSize: '17px',
+  textDecoration: 'none'
+}
+
+
+// ! Need to fix up logout
+const Navbar = () => {
+  return (
+    <div className="navbar" style={styleBar}>
+      <NavLink style={styleLink} to="/logout">Logout</NavLink>
+      <NavLink style={styleLink} to="/leadlists">Dashboard</NavLink>
+      <NavLink style={styleLink} to="/profile">My Profile</NavLink>
+      <NavLink style={styleLink} to="/">Home</NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+// import * as serviceWorker from './serviceWorker';
+import {createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import reducer from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, composeEnhancers(
+//    applyMiddleware(thunk)
+// ));
+
+ReactDOM.render(
+    <App/>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
